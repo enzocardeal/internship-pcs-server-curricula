600d14214c2e96d8297367ad7c438c5f8dd192a6
paulosestini <24728744+paulosestini@users.noreply.github.com>
2023-03-14 22:28:33 -0300

    feat: limit curriculum to one per user

--- src/main/java/com/poli/internship/domain/usecase/CreateCurriculumUseCase.java
+++ src/main/java/com/poli/internship/domain/usecase/CreateCurriculumUseCase.java
@@ -1,9 +1,12 @@
 package com.poli.internship.domain.usecase;
 
+import com.poli.internship.api.error.CustomError;
 import com.poli.internship.data.datasource.CurriculumDataSource;
 import com.poli.internship.data.embeddable.ActivityEmbeddable;
 import com.poli.internship.data.embeddable.ExperienceEmbeddable;
+import com.poli.internship.domain.models.CurriculumModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.execution.ErrorType;
 import org.springframework.stereotype.Service;
 
 import static com.poli.internship.domain.models.CurriculumModel.CurriculumInput;
@@ -18,6 +21,10 @@ public class CreateCurriculumUseCase {
     private CurriculumDataSource dataSource;
 
     public Curriculum exec(CurriculumInput curriculumInput){
+        CurriculumModel.Curriculum curriculum = this.dataSource.getCurriculumByUserId(curriculumInput.userId());
+        if(curriculum != null) {
+            throw new CustomError("Curriculum already exists.", ErrorType.BAD_REQUEST);
+        }
         return this.dataSource.createCurriculum(curriculumInput);
     }
 }
